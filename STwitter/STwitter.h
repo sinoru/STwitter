d8405a0448ec556f286dafe32bb481335d8fabb8
Sinoru <sinoru.dev@me.com>
2011-11-12 12:16:41 +0900

    Initial Commit

---
+++ STwitter/STwitter.h
@@ -0,0 +1,13 @@
+//
+//  STwitter.h
+//  STwitter
+//
+//  Created by 재홍 강 on 11. 11. 12..
+//  Copyright (c) 2011년 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STwitter : NSObject
+
+@end
---
+++ STwitter/STwitter.m
@@ -0,0 +1,23 @@
+//
+//  STwitter.m
+//  STwitter
+//
+//  Created by 재홍 강 on 11. 11. 12..
+//  Copyright (c) 2011년 __MyCompanyName__. All rights reserved.
+//
+
+#import "STwitter.h"
+
+@implementation STwitter
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
