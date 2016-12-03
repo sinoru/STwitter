9d590ec067e035655c4bfc27e066568555925cf2
Jaehong Kang <sinoru@me.com>
2016-12-04 00:42:40 +0900

    create requestVerifyCredential on Account class

--- Sources/Account.swift
+++ Sources/Account.swift
@@ -18,4 +18,59 @@ public class Account: NSObject {
         self.oauthTokenSecret = oauthTokenSecret
         super.init()
     }
+    
+    @objc public class func requestVerifyCredential(session: Session, handler: @escaping (User?, NSError?) -> Void) {
+        guard let url = URL.twitterRESTAPIURL(endpoint: "account/verify_credentials.json") else {
+            handler(nil, Error.unknown)
+            return
+        }
+        
+        let httpMethod = "GET"
+        
+        do {
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = httpMethod
+            
+            let authorizationHeader = try OAuth.authorizationHeader(oauthItems: [:], HTTPMethod: httpMethod, url: url, consumerKey: session.consumerKey, consumerSecret: session.consumerSecret)
+            
+            urlRequest.setValue(authorizationHeader, forHTTPHeaderField: "Authorization")
+            
+            let task = session.urlSession.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
+                if let error = error {
+                    handler(nil, error as NSError?)
+                    return
+                }
+                
+                do {
+                    guard let data = data else {
+                        handler(nil, error as NSError?)
+                        return
+                    }
+                    
+                    let jsonObject = try JSONSerialization.jsonObject(with: data, options: [.allowFragments])
+                    
+                    guard let user = User(jsonObject: jsonObject) else {
+                        handler(nil, error as NSError?)
+                        return
+                    }
+                    
+                    handler(user, nil)
+                }
+                catch let error as NSError {
+                    handler(nil, error)
+                }
+                catch {
+                    handler(nil, Error.unknown)
+                }
+            })
+            task.resume()
+        }
+        catch let error as NSError {
+            handler(nil, error)
+        }
+        catch {
+            handler(nil, Error.unknown)
+        }
+    }
+    
 }
