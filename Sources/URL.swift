d82ccd82c909c1adb9a5943b96ba7333bf12a0c7
Jaehong Kang <sinoru@me.com>
2016-12-03 13:40:36 +0900

    Create URL class

---
+++ Sources/URL.swift
@@ -0,0 +1,19 @@
+//
+//  URL.swift
+//  STwitter
+//
+//  Created by Sinoru on 2016. 12. 3..
+//
+//
+
+import Foundation
+
+extension URL {
+    static func twitterRESTAPIURL(version: String = "1.1", endpoint: String) -> URL? {
+        return self.init(string: endpoint, relativeTo: URL(string: "https://api.twitter.com/\(version)/"))
+    }
+    
+    static func twitterOAuthURL(endpoint: String) -> URL? {
+        return self.init(string: endpoint, relativeTo: URL(string: "https://api.twitter.com/oauth/"))
+    }
+}
