7b851f5fd3dc4992099e0c409a7fdf2e48658e49
Jaehong Kang <sinoru@me.com>
2016-12-05 19:57:59 +0900

    Add a method

--- Sources/CharacterSet.swift
+++ Sources/CharacterSet.swift
@@ -9,9 +9,16 @@
 import Foundation
 
 extension CharacterSet {
+    
     static var urlUnreservedCharacters: CharacterSet {
         var urlUnreservedCharacters = CharacterSet.alphanumerics
         urlUnreservedCharacters.insert(charactersIn: "-._~")
+        return urlUnreservedCharacters
+    }
+    
+    static var twitterAllowedCharacters: CharacterSet{
+        var urlUnreservedCharacters = CharacterSet.alphanumerics
+        urlUnreservedCharacters.insert(charactersIn: "-._~%&=")
         return urlUnreservedCharacters
     }
 }
