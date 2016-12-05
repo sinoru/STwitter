4ae2394c26fec60d5b9dfb02c104dd8f67c11965
Jaehong Kang <sinoru@me.com>
2016-12-05 18:55:24 +0900

    Implement status part

--- Sources/Status.swift
+++ Sources/Status.swift
@@ -9,7 +9,81 @@
 import Foundation
 
 public class Status: NSObject {
-//    class func updateRequestWith(status: String, mediae: [Media]?) throws -> StatusUpdateRequest {
-//        
-//    }
+
+    var jsonObject: [String:Any]
+    
+    public var id: Int64
+    
+    internal init?(jsonObject: Any) {
+        guard let jsonObject = jsonObject as? [String:Any] else {
+            return nil
+        }
+        self.jsonObject = jsonObject
+        
+        guard let id = jsonObject["id"] as? Int64 else {
+            return nil
+        }
+        self.id = id
+        
+        super.init()
+    }
+}
+
+extension Session {
+    
+    public func statusUpdateTask(status: String, possiblySensitive: Bool = false, mediae: [Media]?, completionHandler: @escaping (Status?, Swift.Error?) -> Void) throws -> StatusSessionTask {
+        guard let url = URL.twitterRESTAPIURL(endpoint: "statuses/update.json") else {
+            throw Error.Unknown
+        }
+        
+        let httpMethod = "POST"
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = httpMethod
+        
+        let queryItems = [
+            URLQueryItem(name: "status", value: status),
+            URLQueryItem(name: "possibly_sensitive", value: possiblySensitive ? "true" : "false"),
+        ]
+        
+        let authorizationHeader = try OAuth.authorizationHeader(queryItems: queryItems, HTTPMethod: httpMethod, url: url, consumerKey: self.consumerKey, consumerSecret: self.consumerSecret)
+        urlRequest.setValue(authorizationHeader, forHTTPHeaderField: "Authorization")
+        
+        var urlComponents = URLComponents()
+        urlComponents.queryItems = queryItems
+        
+        urlRequest.httpBody = urlComponents.percentEncodedQuery?.data(using: .utf8)
+        
+        let urlSessionTask = self.urlSession.downloadTask(with: urlRequest, completionHandler: { (location, response, error) in
+            if let error = error {
+                completionHandler(nil, error)
+                return
+            }
+            
+            guard let location = location else {
+                completionHandler(nil, error)
+                return
+            }
+            
+            do {
+                let data = try Data(contentsOf: location)
+                
+                let jsonObject = try JSONSerialization.jsonObject(with: data, options: [.allowFragments])
+                
+                try TwitterError.checkTwitterError(onJsonObject: jsonObject)
+                
+                guard let status = Status(jsonObject: jsonObject) else {
+                    completionHandler(nil, error)
+                    return
+                }
+                
+                completionHandler(status, nil)
+            }
+            catch let error {
+                completionHandler(nil, error)
+            }
+        })
+        
+        return StatusSessionTask(urlSessionTask: urlSessionTask)
+    }
 }
---
+++ Sources/StatusSessionTask.swift
@@ -0,0 +1,13 @@
+//
+//  StatusSessionTask.swift
+//  STwitter
+//
+//  Created by Sinoru on 2016. 12. 4..
+//
+//
+
+import Foundation
+
+public class StatusSessionTask: SessionTask {
+
+}
